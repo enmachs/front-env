9e762e8040496910a455719b58b76522fd4110b4
EnmChs <enmachs@gmail.com>
2017-12-10 18:53:48 -0400

    final stage

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var haml = require('gulp-haml');
 var htmlbeautify = require('gulp-html-beautify');
-
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+var uncss = require('uncss');
 //initialize server
 var browserSync = require('browser-sync').create();
 gulp.task('browserSync', function() {
@@ -12,6 +16,43 @@ gulp.task('browserSync', function() {
     },
   })
 })
+
+
+//Delete dist folder
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+
+
+//Save cache and minify images
+gulp.task('images', function(){
+  return gulp.src('app/img/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/img'))
+});
+
+gulp.task('clean-css', function(){
+  var files = 'dist/*.html',
+  options = {
+    csspath: '',
+    stylesheets: ['css/main.css'],
+    htmlroot: 'dist',
+  };
+  return uncss(files, options, function (error, output) {
+    gulp.dest('dist/css/main.css')
+  });
+});
+
+//Move fonts to dist/fonts
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+});
+
 
 //Compile .scss to .css
 gulp.task('compile_sass', function(){
@@ -42,4 +83,16 @@ gulp.task('watch', ['browserSync', 'compile_haml', 'compile_sass'], function(){
   gulp.watch('app/*.haml', ['compile_haml']);
   gulp.watch('app/js/**/*.js', browserSync.reload) 
   // Other watchers
-})
+});
+
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', 
+    ['compile_haml','compile_sass', 'images', 'fonts']
+  )
+});
+
+gulp.task('default', function (callback) {
+  runSequence(['compile_haml', 'compile_sass','browserSync', 'watch']
+  )
+});
